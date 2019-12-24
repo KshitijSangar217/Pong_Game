f13f71488d41f633196d3274646687668a5767cf
Kshitij Sangar <kshitijsangar@gmail.com>
2019-12-24 11:52:51 +0530

    Very first commit

--- README.md
+++
@@ -1,2 +0,0 @@
-# Pong_Game
-This is a PONG game using python language and pygame package.
---
+++ main_pong.py
@@ -0,0 +1,19 @@
+import pygame
+
+class Bar(object):
+    def __init__(self,x , y = 400,x_size=20,y_size=40):
+        self.x = = 100
+    def move(self):
+        pass
+
+#Colors and RGB tuple...
+white = (255, 255, 255)
+black = (0, 0, 0)
+red = (253,76,76 )
+
+#Game Variables
+run = 1
+
+#Game Loop
+while(run):
+
